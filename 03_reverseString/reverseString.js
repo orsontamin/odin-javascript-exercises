d7fb1f2e057b301f34e825a7bc17d9eccca2282f
Orson <orson.t99@gmail.com>
2021-10-17 09:35:16 +0800

    Pass 02 reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function(str) {
+  let newWord = "";
+  let newStr = str;
 
+  for(let i=newStr.length - 1; i >= 0; i--){
+    newWord += newStr[i];
+  }
+  return newWord;
 };
 
 // Do not edit below this line
